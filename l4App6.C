c989160bab0a0e628b1c6c28ef794741ea65c076
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 06:06:32 +0200

    Update l4App6.C

--- l4App6.C
+++ l4App6.C
@@ -1,1 +1,6 @@
-
+/*En fazla 100 karakterden oluşan dizgileri girdi olarak alınız. Bu dizgiler boş bir karakterle
+birbirinden ayrılmış olan kelimelerden oluşmaktadır. Her dizginin içinde yer alan kelimelerin
+ilk harflerini görüntüleyiniz. Dizgilerin girilme işlemi boş bir dizgi girilinceye kadar devam
+etmelidir. Ayrıca, dizgide yer alan kelimelerin ilk harflerinin bulunmasını bir fonksiyonda
+yapınız. Bu fonksiyon bir dizgiyi parametre olarak almalı ve her kelimenin ilk harfinden oluşan
+kısaltmayı çağrıldığı yere geri döndürmelidir*/
