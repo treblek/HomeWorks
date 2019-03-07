b64d6bd3fee5bd7b379b0501800dd8c5d4a2772b
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 06:06:52 +0200

    Update l4App7.C

--- l4App7.C
+++ l4App7.C
@@ -1,1 +1,5 @@
-
+/*En fazla 100 karakterden oluşan bir cümleyi girdi olarak alınız. Bu cümledeki kelimeler
+birer boşluk karakteri ile birbirinden ayrılmıştır. Ayrıca bu cümlede noktalama işaretleri
+bulunmamaktadır. Bu cümle içinde yer alan en kısa kelimeyi bularak görüntüleyiniz. Bu
+cümle içinde yer alan en kısa kelimeyi bulmayı bir fonksiyonda yapınız. Bu fonksiyon bu
+cümleyi parametre olarak almalı ve en kısa kelimeyi çağrıldığı yere geri döndürmelidir. */
