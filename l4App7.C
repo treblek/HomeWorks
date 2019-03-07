ad7bf25cab29b4ea5f5765f9d2f47bfac540951c
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 11:17:25 +0200

    Update l4App7.C

--- l4App7.C
+++ l4App7.C
@@ -2,4 +2,5 @@
 birer boşluk karakteri ile birbirinden ayrılmıştır. Ayrıca bu cümlede noktalama işaretleri
 bulunmamaktadır. Bu cümle içinde yer alan en kısa kelimeyi bularak görüntüleyiniz. Bu
 cümle içinde yer alan en kısa kelimeyi bulmayı bir fonksiyonda yapınız. Bu fonksiyon bu
-cümleyi parametre olarak almalı ve en kısa kelimeyi çağrıldığı yere geri döndürmelidir. */
+cümleyi parametre olarak almalı ve en kısa kelimeyi çağrıldığı yere geri döndürmelidir. 
+*/
