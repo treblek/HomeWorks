bece0cd74dd524b3d18a8837e4c9ed1dadee27b6
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 06:06:17 +0200

    Update l4App5.C

--- l4App5.C
+++ l4App5.C
@@ -1,1 +1,8 @@
-
+/*En fazla 100 karakterden oluşan bir cümleyi girdi olarak alınız. Bu cümledeki kelimeler
+birer boşluk karakteri ile birbirinden ayrılmıştır. Ayrıca bu cümlede noktalama işaretleri
+bulunmamaktadır. Daha sonra en fazla 10 karakterden oluşan bir kelimeyi de girdi olarak
+alınız. Bu kelimeyi cümle içinde bulup silen ve cümlenin yeni halini ekranda gösterenprogram
+parçasını yazınız.
+Not: Küçük ve büyük harfler farklı karakter olarak değerlendirilmelidir Ayrıca cümle içinde
+aynı kelimeden birden fazla varsa hepsi silinmelidir. Bu kelime başka kelime içinde geçiyorsa
+da silinmelidir. */
