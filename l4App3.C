3445ff12b55623f0aad5af1834e213dcb4eba762
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 06:05:04 +0200

    Update l4App3.C

--- l4App3.C
+++ l4App3.C
@@ -1,1 +1,4 @@
-
+/*4) Kullanıcıdan bir dizgi girmesini isteyen ve daha sonra bu dizgiyi aşağıdaki işi yapan bir
+fonksiyona gönderen ve fonksiyondan gelen cevabı ekranda gösteren bir program yazınız.
+Fonksiyon programdan gelen dizgideki bütün kelimeleri ilk harfleri büyük şekline
+dönüştürerek geri göndermelidir.*/
