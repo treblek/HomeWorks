25164b5537149a300a00db76d0a777995c16309d
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 11:14:27 +0200

    Update l4App5.C

--- l4App5.C
+++ l4App5.C
@@ -1,8 +1,57 @@
-/*En fazla 100 karakterden oluşan bir cümleyi girdi olarak alınız. Bu cümledeki kelimeler
-birer boşluk karakteri ile birbirinden ayrılmıştır. Ayrıca bu cümlede noktalama işaretleri
-bulunmamaktadır. Daha sonra en fazla 10 karakterden oluşan bir kelimeyi de girdi olarak
-alınız. Bu kelimeyi cümle içinde bulup silen ve cümlenin yeni halini ekranda gösterenprogram
-parçasını yazınız.
-Not: Küçük ve büyük harfler farklı karakter olarak değerlendirilmelidir Ayrıca cümle içinde
-aynı kelimeden birden fazla varsa hepsi silinmelidir. Bu kelime başka kelime içinde geçiyorsa
-da silinmelidir. */
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdint.h"
+//Sadece ilk bulduğunu siler.
+uint8_t karakterSayisi(char*); // Dizi karakter sayısını dönderen fonksiyon
+void bulVeSil(char*,char*);// Bulma ve silme işlemini yapan fonksiyon
+int main(void)
+{
+	printf("Lutfen cumle giriniz.");
+	char *karakterDizisi,*silinecekKarakterDizisi;
+	gets(karakterDizisi);
+	printf("Lutfen silinecek kelimeyi giriniz. ");
+	gets(silinecekKarakterDizisi);
+	system("cls");
+	printf("Eski Hali :%s\n",karakterDizisi);
+	bulVeSil(karakterDizisi,silinecekKarakterDizisi);
+	printf("Yeni Hali %s",karakterDizisi);
+		return -1;
+}
+void bulVeSil(char*karakterDizisi,char*silinecekKarakterDizisi)
+{
+	uint8_t donguDegiskeni,ikinciDonguDegiskeni,yardimciDegisken=0;
+	for(donguDegiskeni=0;donguDegiskeni<karakterSayisi(karakterDizisi)-karakterSayisi(silinecekKarakterDizisi);donguDegiskeni++)
+	{
+		for(ikinciDonguDegiskeni=0;ikinciDonguDegiskeni<karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+		{
+			if(*(karakterDizisi+donguDegiskeni+ikinciDonguDegiskeni) ==*(silinecekKarakterDizisi+ikinciDonguDegiskeni))
+			{
+				yardimciDegisken++;
+			}
+		}
+		if(yardimciDegisken== karakterSayisi(silinecekKarakterDizisi))
+		{
+				for(ikinciDonguDegiskeni=0;ikinciDonguDegiskeni<karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+		{
+			*(karakterDizisi+donguDegiskeni+ikinciDonguDegiskeni) =*(silinecekKarakterDizisi+ikinciDonguDegiskeni);
+		}
+		break;
+		}
+		yardimciDegisken =0;
+	}
+	for(ikinciDonguDegiskeni = donguDegiskeni;ikinciDonguDegiskeni<=karakterSayisi(karakterDizisi)-karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+	{
+		*(karakterDizisi+ikinciDonguDegiskeni) = *(karakterDizisi+ikinciDonguDegiskeni+karakterSayisi(silinecekKarakterDizisi));
+		*(karakterDizisi+ikinciDonguDegiskeni+karakterSayisi(silinecekKarakterDizisi)) = '0';
+	}
+}
+uint8_t karakterSayisi(char*karakterDizisi)
+{
+	uint8_t donguDegiskeni=0;
+	while(*(karakterDizisi+donguDegiskeni) != '\0')
+	{
+		donguDegiskeni++;
+	}
+	return donguDegiskeni;
+}
