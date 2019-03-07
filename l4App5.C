43256d18814b7fb6e507b2306b3d6ea1c52515bf
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-07 11:14:53 +0200

    Update l4App5.C

--- l4App5.C
+++ l4App5.C
@@ -1,3 +1,10 @@
+/*En fazla 100 karakterden oluşan dizgileri girdi olarak alınız. Bu dizgiler boş bir karakterle
+birbirinden ayrılmış olan kelimelerden oluşmaktadır. Her dizginin içinde yer alan kelimelerin
+ilk harflerini görüntüleyiniz. Dizgilerin girilme işlemi boş bir dizgi girilinceye kadar devam
+etmelidir. Ayrıca, dizgide yer alan kelimelerin ilk harflerinin bulunmasını bir fonksiyonda
+yapınız. Bu fonksiyon bir dizgiyi parametre olarak almalı ve her kelimenin ilk harfinden oluşan
+kısaltmayı çağrıldığı yere geri döndürmelidir.*/
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdint.h"
